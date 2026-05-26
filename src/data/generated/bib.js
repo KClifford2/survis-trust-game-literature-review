ebb47d18f963d07e3ba314a128ec72a3aef337cc
KClifford2 <94976067+KClifford2@users.noreply.github.com>
2026-05-26 13:35:12 +0000

    Generate SurVis data

--- src/data/generated/bib.js
+++ src/data/generated/bib.js
@@ -1,17 +1,113 @@
 ﻿const generatedBibEntries = {
-    "Beck2016Visual": {
-        "abstract": "Bibiographic data such as collections of scientific articles and citation networks have been studied extensively in information visualization and visual analytics research. Powerful systems have been built to support various types of bibliographic analysis, but they require some training and cannot be used to disseminate the insights gained. In contrast, we focused on developing a more accessible visual analytics system, called SurVis, that is ready to disseminate a carefully surveyed literature collection. The authors of a survey may use our Web-based system to structure and analyze their literature database. Later, readers of the survey can obtain an overview, quickly retrieve specific publications, and reproduce or extend the original bibliographic analysis. Our system employs a set of selectors that enable users to filter and browse the literature collection as well as to control interactive visualizations. The versatile selector concept includes selectors for textual search, filtering by keywords and meta-information, selection and clustering of similar publications, and following citation links. Agreement to the selector is represented by word-sized sparkline visualizations seamlessly integrated into the user interface. Based on an analysis of the analytical reasoning process, we derived requirements for the system. We developed the system in a formative way involving other researchers writing literature surveys. A questionnaire study with 14 visual analytics experts confirms that SurVis meets the initially formulated requirements.",
-        "author": "Beck, Fabian and Koch, Sebastian and Weiskopf, Daniel",
-        "doi": "10.1109/TVCG.2015.2467757",
-        "journal": "IEEE Transactions on Visualization and Computer Graphics",
-        "keywords": "type:system, visual_analytics, sparklines, information_retrieval, clustering, literature_browser",
-        "number": "01",
-        "publisher": "IEEE",
-        "series": "TVCG",
-        "title": "Visual Analysis and Dissemination of Scientific Literature Collections with {SurVis}",
+    "chetty2021trust": {
+        "author": "Chetty, Rinelle and Hofmeyr, Andre and Kincaid, Harold and Monroe, Brian",
+        "doi": "10.1016/j.socec.2020.101520",
+        "journal": "Journal of Behavioral and Experimental Economics",
+        "keywords": "trust-game-measurement, risk-trust-confound, risk",
+        "pages": "101520",
+        "title": "The Trust Game Does Not (Only) Measure Trust: The Risk-Trust Confound Revisited",
         "type": "article",
-        "url": "http://www.visus.uni-stuttgart.de/uploads/tx_vispublications/vast15-survis.pdf",
-        "volume": "22",
-        "year": "2016"
+        "volume": "90",
+        "year": "2021"
+    },
+    "dcruz2025trust": {
+        "author": "D'Cruz, Jason R.",
+        "doi": "doi.org/10.1007/s10551-025-06117-3",
+        "journal": "Journal of Business Ethics",
+        "keywords": "trust-game-measurement, trustworthiness, critique",
+        "title": "What Does the Trust Game Measure?",
+        "type": "article",
+        "year": "2025"
+    },
+    "dhakal2022evolution": {
+        "author": "Dhakal, Sandeep and Chiong, Raymond and Chica, Manuel and Han, The Anh",
+        "doi": "10.1098/rsos.212000",
+        "journal": "Royal Society Open Science",
+        "keywords": "cooperation-social-dilemma, trust, migration, tags",
+        "pages": "212000",
+        "title": "Evolution of cooperation and trust in an N-player social dilemma game with tags for migration decisions",
+        "type": "article",
+        "volume": "9",
+        "year": "2022"
+    },
+    "frederiksen2012dimensions": {
+        "author": "Frederiksen, Morten",
+        "doi": "10.1177/0011392112461800",
+        "journal": "Current Sociology",
+        "keywords": "conceptual-trust, intersubjective-trust, sociology",
+        "number": "6",
+        "pages": "733--750",
+        "title": "Dimensions of trust: An empirical revisit to Simmel's formal sociology of intersubjective trust",
+        "type": "article",
+        "volume": "60",
+        "year": "2012"
+    },
+    "johnson2011trust": {
+        "author": "Johnson, Noel D. and Mislin, Alexandra A.",
+        "doi": "10.1016/j.joep.2011.05.007",
+        "journal": "Journal of Economic Psychology",
+        "keywords": "trust-game-measurement, meta-analysis, trust-game",
+        "number": "5",
+        "pages": "865--889",
+        "title": "Trust games: A meta-analysis",
+        "type": "article",
+        "volume": "32",
+        "year": "2011"
+    },
+    "kelp2023trustworthiness": {
+        "author": "Kelp, Christoph and Simion, Mona",
+        "doi": "10.1111/nous.12448",
+        "journal": "No{\\^u}s",
+        "keywords": "conceptual-trust, trustworthiness, obligations",
+        "number": "3",
+        "pages": "667--683",
+        "title": "What is trustworthiness?",
+        "type": "article",
+        "volume": "57",
+        "year": "2023"
+    },
+    "michailidis2018flow": {
+        "author": "Michailidis, Lazaros and Balaguer-Ballester, Emili and He, Xun",
+        "doi": "10.3389/fpsyg.2018.01682",
+        "journal": "Frontiers in Psychology",
+        "keywords": "flow-game-experience, flow, immersion",
+        "pages": "1682",
+        "title": "Flow and Immersion in Video Games: The Aftermath of a Conceptual Challenge",
+        "type": "article",
+        "volume": "9",
+        "year": "2018"
+    },
+    "muller2020big": {
+        "author": "M{\\\"u}ller, Julia and Schwieren, Christiane",
+        "doi": "10.1007/s11573-019-00928-3",
+        "journal": "Journal of Business Economics",
+        "keywords": "trust-game-measurement, personality, big-five",
+        "pages": "37--55",
+        "title": "Big Five personality factors in the Trust Game",
+        "type": "article",
+        "volume": "90",
+        "year": "2020"
+    },
+    "perttula2017flow": {
+        "author": "Perttula, Arttu and Kiili, Kristian and Lindstedt, Antero and Tuomi, Pauliina",
+        "doi": "10.17083/ijsg.v4i1.151",
+        "journal": "International Journal of Serious Games",
+        "keywords": "flow-game-experience, serious-games, game-based-learning",
+        "number": "1",
+        "title": "Flow experience in game based learning: A systematic literature review",
+        "type": "article",
+        "volume": "4",
+        "year": "2017"
+    },
+    "verheijen2019influence": {
+        "author": "Verheijen, Geert P. and Stoltz, Sabine E. M. J. and van den Berg, Yvonne H. M. and Cillessen, Antonius H. N.",
+        "doi": "doi.org/10.1016/j.chb.2018.10.023",
+        "journal": "Computers in Human Behavior",
+        "keywords": "cooperation-social-dilemma, cooperative-play, competitive-play",
+        "pages": "297--304",
+        "title": "The influence of competitive and cooperative video games on behavior during play and friendship quality in adolescence",
+        "type": "article",
+        "volume": "91",
+        "year": "2019"
     }
 };
